ed46c55fe6129d6501c159aefbd9f43a987f17ba
Ocean <mintteacupofficial@gmail.com>
2025-06-03 00:33:34 +0100

    comment

--- static/map.js
+++ static/map.js
@@ -3,4 +3,4 @@ var map = L.map('map').setView([51.505, -0.09], 13); // Looks for div with the i
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { // pulls map images from openstreetmap
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map); // attaches image to map

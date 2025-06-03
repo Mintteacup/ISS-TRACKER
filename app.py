c00d9b05efd38bded61e5b89dcb2e2a6ad630b85
Ocean <mintteacupofficial@gmail.com>
2025-06-03 01:27:45 +0100

    got the location of the iss

--- app.py
+++ app.py
@@ -9,5 +9,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/iss-location")
+def iss_location():
+    
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    
+    data = response.json()
+    
+    lat = float(data["iss_position"]["latitude"])
+    lon = float(data["iss_position"]["longitude"])
+
+    return jsonify({"lat": lat, "lon": lon})
+
+
 if __name__ == "__main__":
     app.run(debug=True)

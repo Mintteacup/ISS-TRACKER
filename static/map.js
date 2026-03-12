b8addc298bdcd824cab851639d204187e02e72b2
Ocean <mintteacupofficial@gmail.com>
2026-03-12 13:15:19 +0000

    Removed old markers to prevent icon overlap

--- static/map.js
+++ static/map.js
@@ -1,11 +1,20 @@
 var map = L.map('map').setView([0, 0], 2); // Looks for div with the id map, then centres the map on specific lat and lon and then zoom level
 
+let currentMarker = null;
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { // pulls map images from openstreetmap
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map); // attaches image to map
 
 setInterval(update, 5000); // 5 seconds
+
+var issIcon = L.icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/International_Space_Station.svg',
+    iconSize: [120, 120], // width, height in pixels
+    iconAnchor: [60, 60] // point of the icon that corresponds to marker's location
+});
+
 
 function update() {
   fetch("/iss-location") // calls the flask back end
@@ -14,11 +23,19 @@ function update() {
     const lat = data.lat;
     const lon = data.lon;
     console.log(lat, lon)
-    L.marker([lat, lon]).bindPopup(
-      "<b>ISS Location</b><br>" +
-      "Lat: " + lat.toFixed(4) + "<br>" +
-      "Lon: " + lon.toFixed(4)
-    ).openPopup().addTo(map); // puts a leaflet marker where the iss is
+
+    if (currentMarker) {
+      map.removeLayer(currentMarker);
+    }
+
+    currentMarker = L.marker([lat, lon], {icon: issIcon})
+      .bindPopup(
+        "<b>ISS Location</b><br>" +
+        "Lat: " + lat.toFixed(4) + "<br>" +
+        "Lon: " + lon.toFixed(4)
+      )
+      .openPopup()
+      .addTo(map); // puts a leaflet marker where the iss is
 
     map.setView([lat, lon], 4); // centers the map on the ISS
   });
